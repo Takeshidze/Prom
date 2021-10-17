9930e39391091edf883281315371960c5dee572b
Takeshidze <77339480+Takeshidze@users.noreply.github.com>
2021-10-17 14:44:14 +0300

    задача с двигающейся платформой

--- run.py
+++ run.py
@@ -1,38 +1,34 @@
-from random import randint
+# https://github.com/Takeshidze/Prom/tree/game
 import pygame
 import sys
 
 def main():
+    exit = False
     size = width, height = 800, 600
     BG_COLOR = (0, 0, 0)
-    exit = False
-    logo = pygame.image.load('basketball.png')
-    logo = pygame.transform.scale(logo, (100, 100))
-    img_size = logo.get_rect().size
     screen = pygame.display.set_mode(size, pygame.RESIZABLE)
-    x = randint(50, width-100)
-    y = randint(50, height-100)
-    x_speed = 2.5
-    y_speed = 2.5
-    def move(x, y):
-        screen.blit(logo, (x, y))
+    paddle_w = 60
+    paddle_h = 15
+    paddle_speed = 10
+    paddle = pygame.Rect(width // 2 - paddle_w // 2, height - paddle_h - 10, paddle_w, paddle_h)
     while exit == False:
         screen.fill(BG_COLOR)
-        if (x + img_size[0] >= width) or (x <= 0):
-            x_speed = -x_speed
-        if (y + img_size[1] >= height) or (y <= 0):
-            y_speed = -y_speed
-        x += x_speed
-        y += y_speed
-        move(x, y)
-        pygame.display.flip()
-        pygame.time.wait(10)
+        pygame.draw.rect(screen, pygame.Color('darkorange'), paddle)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit = True
             if event.type == pygame.VIDEORESIZE:
                 size = width, height = event.w, event.h
                 screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+            key = pygame.key.get_pressed()
+            if key[pygame.K_a] and paddle.left > 0:
+                paddle.left -= paddle_speed
+            if key[pygame.K_d] and paddle.right < width:
+                paddle.right += paddle_speed
+        pygame.display.flip()
+        pygame.time.wait(10)
     sys.exit()
+
+
 if __name__ == '__main__':
     main()
